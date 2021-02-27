277ee83bfa46fc4283c19acef1260e73773c44ca
Sandro Huber <sandrochuber@gmail.com>
2021-02-27 13:04:12 +0100

    Version 0.0.2.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pylic",
-    version="0.0.1",
+    version="0.0.2",
     author="Sandro Huber",
     author_email="sandrochuber@gmail.com",
     description="Python license checker",
