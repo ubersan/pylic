c86a48c392f78a5202390eee6df673c854f6e66d
Sandro Huber <sandrochuber@gmail.com>
2021-03-02 22:33:56 +0100

    Add minor version classifiers.

--- setup.py
+++ setup.py
@@ -23,6 +23,10 @@ setuptools.setup(
     install_requires=["toml", "importlib-metadata"],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
