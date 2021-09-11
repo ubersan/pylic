c52c59f2ef0e1a0b7bb78d5495ea70d06241a886
sandro <sandrochuber@gmail.com>
2021-09-11 13:24:32 +0200

    remove old main in pylic.py

--- pylic/pylic.py
+++ pylic/pylic.py
@@ -133,17 +133,3 @@ def check_licenses(safe_licenses: List[str], installed_licenses: List[dict]) -> 
         return False
 
     return True
-
-
-def main() -> None:
-    safe_licenses, unsafe_packages = read_config()
-    installed_licenses = read_all_installed_licenses_metadata()
-    no_unnecessary_safe_licenses = check_for_unnecessary_safe_licenses(safe_licenses, installed_licenses)
-    no_unncessary_unsafe_packages = check_for_unnecessary_unsafe_packages(unsafe_packages, installed_licenses)
-    packages_ok = check_unsafe_packages(unsafe_packages, installed_licenses)
-    licenses_ok = check_licenses(safe_licenses, installed_licenses)
-
-    if all([no_unnecessary_safe_licenses, no_unncessary_unsafe_packages, packages_ok, licenses_ok]):
-        print("All licenses ok")
-    else:
-        sys.exit(1)
