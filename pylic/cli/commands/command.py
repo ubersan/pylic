ee2e1ea12e2b9d69bbadc98d2036a616fe500550
sandro <sandrochuber@gmail.com>
2022-04-23 23:22:09 +0200

    fix list for 3.8 3.7.

--- pylic/cli/commands/command.py
+++ pylic/cli/commands/command.py
@@ -1,6 +1,9 @@
+from typing import List
+
+
 class Command:
-    targets: list[str]
+    targets: List[str]
     token: str
 
-    def handle(self, options: list[str]) -> int:
+    def handle(self, options: List[str]) -> int:
         raise NotImplementedError()
